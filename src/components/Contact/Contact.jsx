835d313b5fd5b00135df01f27ddb894dd1bf4c2d
Ogooluwa <ogooluwaakinleye29@gmail.com>
2026-03-22 07:20:27 -0700

    updated form contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -35,19 +35,19 @@ function Contact(){
 
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">Name</label>
-            <input type="text"  className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+            <input type="text" name='name'  className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
             placeholder="Enter Your Name" required/>
           </div>
 
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
-            <input type="email"  className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+            <input type="email" name='email'  className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
             placeholder="Enter Your Email" required/>
           </div>
 
            <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">Message</label>
-            <textarea type="email"  className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+            <textarea type="email" name='message'  className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
             placeholder="Write Your Message...." required rows="4"></textarea>
           </div>
           <button type="submit" className="w-full bg-indigo-600 text-white font-semibold py-3 rounded-lg shadow hover:bg-indigo-700 transition">Send Message</button>
